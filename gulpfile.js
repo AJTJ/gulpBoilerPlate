1a79c2ac33f5c83eec723e4850c10485d7cf0f46
Aaron Janke <janke.aaron@gmail.com>
2018-03-08 11:58:53 -0500

    added notify, plumber etc

--- gulpfile.js
+++ gulpfile.js
@@ -7,26 +7,34 @@ const browserSync = require('browser-sync').create();
 const reload = browserSync.reload;
 const minifyCSS = require('gulp-minify-css');
 const uglify = require('gulp-uglify');
-// notify = require('gulp-notify'),
-// plumber = require('gulp-plumber'),
-// sourcemaps = require('gulp-sourcemaps'),
+const notify = require('gulp-notify');
+const plumber = require('gulp-plumber');
+sourcemaps = require('gulp-sourcemaps'),
 
 gulp.task('styles', () => {
    return gulp.src('./dev/styles/style.scss')
+      .pipe(plumber({
+         errorHandler: notify.onError("Error: <%= error.message %>")
+      }))
+      .pipe(sourcemaps.init())
       .pipe(sass().on('error', sass.logError))
-      .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
       .pipe(minifyCSS())
       .pipe(concat('style.css'))
+      .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+      .pipe(sourcemaps.write('.'))
       .pipe(gulp.dest('./public/styles'))
       .pipe(reload({ stream: true }));
 });
 
 gulp.task('scripts', () => {
    return gulp.src('./dev/scripts/main.js')
+      .pipe(plumber({
+         errorHandler: notify.onError("Error: <%= error.message %>")
+      }))
       .pipe(babel({
          presets: ['env']
       }))
-      .pipe(concat('main.js'))
+      .pipe(concat('main.min.js'))
       .pipe(uglify())
       .pipe(gulp.dest('./public/scripts'))
       .pipe(reload({ stream: true }));
